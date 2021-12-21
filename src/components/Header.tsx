6f3d2fab152e8935cf164b872b18edbf2ed4b358
chenzelig <czelig@zscaler.com>
2021-12-21 15:45:49 +0200

    header, favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
-import './Header.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-function App() {
+export default function ButtonAppBar() {
     return (
-        <div>
-            header
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Team.me
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
-
-export default App;
